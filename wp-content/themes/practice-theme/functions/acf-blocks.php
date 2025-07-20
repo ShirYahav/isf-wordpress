1c8a272dc23b418fa8a0a1157ee434327f500bc4
Shir Yahav <shiryahav07@gmail.com>
2025-07-20 15:21:54 +0300

    finished 'shaping the newspace' section

--- wp-content/themes/practice-theme/functions/acf-blocks.php
+++ wp-content/themes/practice-theme/functions/acf-blocks.php
@@ -14,5 +14,6 @@ function practice_theme_register_acf_blocks()
     register_block_type(get_template_directory() . '/template-parts/blocks/join-us');
     register_block_type(get_template_directory() . '/template-parts/blocks/logo-stripe');
     register_block_type(get_template_directory() . '/template-parts/blocks/our-mission');
+    register_block_type(get_template_directory() . '/template-parts/blocks/feature-banner');
 }
 add_action('init', 'practice_theme_register_acf_blocks');
